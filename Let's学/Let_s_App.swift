56fdc99c50fe90a1e2a18b8d8b6a20fa6192fb91
de <weixun.qian98@gmail.com>
2026-01-04 10:18:35 -0600

    Initial Commit

---
+++ Let's学/Let_s_App.swift
@@ -0,0 +1,32 @@
+//
+//  Let_s_App.swift
+//  Let's学
+//
+//  Created by Qian dede on 1/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Let_s_App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Let's学Tests/Let_s_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Let_s_Tests.swift
+//  Let's学Tests
+//
+//  Created by Qian dede on 1/4/26.
+//
+
+import Testing
+
+struct Let_s_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
